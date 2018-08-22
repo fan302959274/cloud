5556180b9443939b3c08e5f6d8530930a6107bc0
sh00859 <302959274@qq.com>
2018-08-22 19:41:26 +0800

    增加layui前端项目

---
+++ cloud-web/src/main/java/com/cloud/web/WebApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.cloud.web"})
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
