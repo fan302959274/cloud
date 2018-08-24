aef68377e017b36f33d2e17e586902fdbc17c40d
sh00859 <302959274@qq.com>
2018-08-24 14:50:39 +0800

    zipkin使用注意

--- cloud-web/src/main/java/com/cloud/web/WebApplication.java
+++ cloud-web/src/main/java/com/cloud/web/WebApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@ComponentScan(basePackages = {"com.cloud.web"})
 public class WebApplication {
 
     public static void main(String[] args) {
