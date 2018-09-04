75d993920392d009c1bb1e96cf1dd5fdc436556d
cff <302959274@qq.com>
2018-09-04 23:38:08 +0800

    业务id和序列号增加集群策略处理

--- cloud-core/src/main/java/com/cloud/core/service/impl/CommonServiceImpl.java
+++ cloud-core/src/main/java/com/cloud/core/service/impl/CommonServiceImpl.java
@@ -5,6 +5,7 @@ import com.cloud.core.service.CommonService;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.support.atomic.RedisAtomicLong;
 import org.springframework.stereotype.Service;
@@ -17,17 +18,19 @@ public class CommonServiceImpl implements CommonService {
     Logger logger = LoggerFactory.getLogger(this.getClass());
     @Resource
     RedisTemplate<Object, Object> redisTemplate;
+    @Value("${redis.master.number}")
+    String clusterNum;
 
     @Override
     public String getBusinessId() {
         RedisAtomicLong counter = new RedisAtomicLong("businessdata", redisTemplate.getConnectionFactory());
-        return DateUtil.format(new Date(), DateUtil.YYYYMMDD) + StringUtils.leftPad(counter.incrementAndGet() + "", 10, "0");
+        return DateUtil.format(new Date(), DateUtil.YYYYMMDD) + StringUtils.leftPad(counter.getAndAdd(Long.parseLong(clusterNum)) + "", 10, "0");
     }
 
     @Override
     public String getSerialNo(String type) {
         RedisAtomicLong counter = new RedisAtomicLong("serialdata", redisTemplate.getConnectionFactory());
-        String result = (StringUtils.isBlank(type) ? "serial" : type) + StringUtils.leftPad(counter.incrementAndGet() + "", 10, "0");
+        String result = (StringUtils.isBlank(type) ? "serial" : type) + StringUtils.leftPad(counter.getAndAdd(Long.parseLong(clusterNum)) + "", 10, "0");
         return result;
     }
 }
